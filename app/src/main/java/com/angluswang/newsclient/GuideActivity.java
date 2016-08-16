3091498b69703a17967240182b236e385a242838
AnglusWang <AnglusWang@163.com>
2016-08-16 22:48:19 +0800

    向导页 --- 红色圆点位置 跟随手指移动

--- app/src/main/java/com/angluswang/newsclient/GuideActivity.java
+++ app/src/main/java/com/angluswang/newsclient/GuideActivity.java
@@ -6,9 +6,11 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -24,11 +26,12 @@ public class GuideActivity extends Activity {
 
     private ViewPager vpGuide;
     private ArrayList<ImageView> iv_List;
-    private GuideAdapter adapter;
 
-    private Button btnStart;// 开始体验
+    private Button btnStart; // 开始体验
     private LinearLayout llPointGroup;
     private View viewRedPoint;
+
+    private int mPointWidth; // 圆点之间的距离
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +56,8 @@ public class GuideActivity extends Activity {
             iv_List.add(image);
         }
 
-        adapter = new GuideAdapter();
-        vpGuide.setAdapter(adapter);
+        vpGuide.setAdapter(new GuideAdapter());
+        vpGuide.setOnPageChangeListener(new GuideListener());
 
         // 初始化引导页的小圆点
         for (int i = 0; i < iv_Ids.length; i++) {
@@ -71,18 +74,34 @@ public class GuideActivity extends Activity {
             llPointGroup.addView(point); // 将圆点添加给线性布局
         }
 
+        // 获取视图树, 对layout结束事件进行监听
+        llPointGroup.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+
+                    // 当layout执行结束后回调此方法
+                    @Override
+                    public void onGlobalLayout() {
+                        llPointGroup.getViewTreeObserver()
+                                .removeGlobalOnLayoutListener(this);
+                        mPointWidth = llPointGroup.getChildAt(1).getLeft()
+                                - llPointGroup.getChildAt(0).getLeft();
+                    }
+                });
+
     }
 
+    /**
+     * ViewPager 适配器
+     */
     private class GuideAdapter extends PagerAdapter {
-
         @Override
         public int getCount() {
             return iv_List.size();
         }
 
         @Override
-        public boolean isViewFromObject(View arg0, Object arg1) {
-            return arg0 == arg1;
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
         }
 
         @Override
@@ -94,6 +113,43 @@ public class GuideActivity extends Activity {
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
+        }
+    }
+
+    /**
+     * ViewPager 滑动监听
+     */
+    private class GuideListener implements ViewPager.OnPageChangeListener {
+
+        // 滑动事件
+        @Override
+        public void onPageScrolled(int position, float positionOffset,
+                                   int positionOffsetPixels) {
+            int len = (int) (mPointWidth * positionOffset) + position * mPointWidth;
+            // 获取当前红点的布局参数
+            RelativeLayout.LayoutParams params =
+                    (RelativeLayout.LayoutParams) viewRedPoint.getLayoutParams();
+            params.leftMargin = len; // 设置左边距
+
+            viewRedPoint.setLayoutParams(params); // 重新给小红点设置布局参数
+        }
+
+        // 某页面被选中
+        @Override
+        public void onPageSelected(int position) {
+
+            // 开始体验 按钮处理
+            if (position == iv_Ids.length - 1) {
+                btnStart.setVisibility(View.VISIBLE);
+            } else {
+                btnStart.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        // 滑动状态发送改变
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
         }
     }
 }
