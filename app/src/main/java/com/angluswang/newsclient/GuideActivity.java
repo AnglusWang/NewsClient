a65b09353a6384f4aaad193413809d0034da6416
AnglusWang <AnglusWang@163.com>
2016-08-16 17:22:03 +0800

    向导页 --- 添加向导页小圆点

--- app/src/main/java/com/angluswang/newsclient/GuideActivity.java
+++ app/src/main/java/com/angluswang/newsclient/GuideActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
@@ -18,14 +19,16 @@ import java.util.ArrayList;
 
 public class GuideActivity extends Activity {
 
-    private static final int[] iv_Ids = new int[] { R.drawable.guide_1,
-            R.drawable.guide_2, R.drawable.guide_3 };
+    private static final int[] iv_Ids = new int[]{R.drawable.guide_1,
+            R.drawable.guide_2, R.drawable.guide_3};
 
     private ViewPager vpGuide;
     private ArrayList<ImageView> iv_List;
     private GuideAdapter adapter;
 
     private Button btnStart;// 开始体验
+    private LinearLayout llPointGroup;
+    private View viewRedPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,8 @@ public class GuideActivity extends Activity {
 
         vpGuide = (ViewPager) findViewById(R.id.vp_guide);
         btnStart = (Button) findViewById(R.id.btn_start);
+        llPointGroup = (LinearLayout) findViewById(R.id.ll_point_group);
+        viewRedPoint = (View) findViewById(R.id.view_red_point);
 
         // 初始化引导页的3个页面
         iv_List = new ArrayList<>();
@@ -50,6 +55,21 @@ public class GuideActivity extends Activity {
 
         adapter = new GuideAdapter();
         vpGuide.setAdapter(adapter);
+
+        // 初始化引导页的小圆点
+        for (int i = 0; i < iv_Ids.length; i++) {
+            View point = new View(this);
+            point.setBackgroundResource(R.drawable.shape_point_gray); // 设置引导页默认圆点
+
+            LinearLayout.LayoutParams params =
+                    new LinearLayout.LayoutParams(10, 10);
+            if (i > 0) {
+                params.leftMargin = 10; // 设置圆点间隔
+            }
+            point.setLayoutParams(params); // 设置圆点的大小
+
+            llPointGroup.addView(point); // 将圆点添加给线性布局
+        }
 
     }
 
