0327f5bf3459bb69af29d55e5640d669ee5afa36
AnglusWang <AnglusWang@163.com>
2016-08-19 16:11:24 +0800

    引入 xutils 与 gson 库， 从 Tomcat 服务器上获取数据

--- app/src/main/java/com/angluswang/newsclient/base/impl/NewsCenterPager.java
+++ app/src/main/java/com/angluswang/newsclient/base/impl/NewsCenterPager.java
@@ -2,10 +2,20 @@ package com.angluswang.newsclient.base.impl;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.angluswang.newsclient.base.BasePager;
+import com.angluswang.newsclient.bean.NewsData;
+import com.angluswang.newsclient.global.GlobalContants;
+import com.google.gson.Gson;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 
 /**
  * Created by AnglusWang on 2016/8/19.
@@ -13,6 +23,8 @@ import com.angluswang.newsclient.base.BasePager;
  */
 
 public class NewsCenterPager extends BasePager {
+
+    private NewsData mNewsData;
 
     public NewsCenterPager(Activity activity) {
         super(activity);
@@ -32,5 +44,44 @@ public class NewsCenterPager extends BasePager {
 
         // 向 FrameLayout 中动态添加布局
         flContent.addView(text);
+
+        getDataFromService();
+    }
+
+    /**
+     * 从服务器 获取数据
+     */
+    private void getDataFromService() {
+
+        HttpUtils http = new HttpUtils();
+        http.send(HttpRequest.HttpMethod.GET, GlobalContants.CATEGORIES_URL,
+                new RequestCallBack<String>() {
+                    @Override
+                    public void onSuccess(ResponseInfo<String> responseInfo) {
+                        String result = responseInfo.result;
+                        Log.i("返回结果:", result);
+
+                        parseData(result);
+                    }
+
+                    @Override
+                    public void onFailure(HttpException error, String msg) {
+                        Toast.makeText(mActivity, msg,
+                                Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                });
+    }
+
+    /**
+     * 解析 服务器返回的 json 数据
+     *
+     * @param result
+     */
+    private void parseData(String result) {
+
+        Gson gson = new Gson();
+        mNewsData = gson.fromJson(result, NewsData.class);
+        Log.i("解析结果:", mNewsData.toString());
     }
 }
---
+++ app/src/main/java/com/angluswang/newsclient/global/GlobalContants.java
@@ -0,0 +1,14 @@
+package com.angluswang.newsclient.global;
+
+/**
+ * Created by AnglusWang on 2016/8/19.
+ * 定义全局参数
+ */
+
+public class GlobalContants {
+
+    // 使用 Genymotion 模拟器时，为 10.0.3.2，其他模拟器 10.0.2.2
+    public static final String SERVER_URL = "http://10.0.3.2:8080/zhbj/";
+    public static final String CATEGORIES_URL =
+            SERVER_URL + "categories.json"; // 获取分类信息的接口
+}
